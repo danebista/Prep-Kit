e0f2c3f9da87547588cc846f53e34c4ff5438475
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2022-09-30 01:33:50 -0400

    output-contest-matches

--- Two Sum/src/879. OutputContestMatches.java
+++ Two Sum/src/879. OutputContestMatches.java
@@ -23,5 +23,36 @@ public class Solution {
             match[i] = "(" + match[i] + "," + match[n - i - 1] + ")";
         }
         helper(n / 2);
-    }
+    }}
+
+Alternate Route
+
+    public class Solution {
+        /**
+         * @param n: a integer, denote the number of teams
+         * @return: a string
+         */
+        public String findContestMatch(int n) {
+            // write your code here
+            return helper(n, n).get(0);
+        }
+
+        public List<String> helper(int n, int m) {
+            List<String> result = new ArrayList();
+
+            if (n <= 2) {
+                for (int i = 0; i < m / 2; i++) {
+                    result.add("(" + (i + 1) + "," + (m - i) + ")");
+                }
+                return result;
+            }
+            List<String> values = helper(n / 2, m);
+
+            for (int i = 0; i < values.size() / 2; i++) {
+                String val = "(" + values.get(i) + "," + values.get(values.size() - i - 1) + ")";
+                result.add(val);
+            }
+
+            return result;
+        }
 }
