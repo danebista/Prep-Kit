6d8bc99224995fb444b293e049f51b4bb45a4439
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2022-10-04 06:46:45 -0400

    Some triangles problem

--- Two Sum/src/120. Triangles.java
+++ Two Sum/src/120. Triangles.java
@@ -4,12 +4,13 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         for (int i = triangle.size() - 2; i >= 0; i--) {
             for (int j = 0; j < triangle.get(i).size(); j++) {
-                int temp = triangle.get(i).get(j)
-                        + Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1));
-                triangle.get(i).remove(j);
-                triangle.get(i).add(j, temp);
+
+                triangle.get(i).set(j, triangle.get(i).get(j) + Math.min(
+                        triangle.get(i + 1).get(j),
+                        triangle.get(i + 1).get(j + 1)));
             }
         }
+
         return triangle.get(0).get(0);
     }
 }
