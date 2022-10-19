d334fa330a8d7b92b4ea2c399380a343618bc5a9
Dinank Bista <dinankbista@endpoint-10-192-213-10.ufvisitor.ufl.edu>
2022-10-19 13:34:57 -0400

    Palindorme Partitioning

--- PrepKit-Java/src/132. PalindromePartitioningII.java
+++ PrepKit-Java/src/132. PalindromePartitioningII.java
@@ -38,3 +38,34 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    public int minCut(String s) {
+        int[] dp = new int[s.length()+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[s.length()] = 0;
+        
+        for (int i= s.length()-1; i>=0; i--){
+            for (int j=i; j< s.length(); j++){
+                if (isPalindrome(s, i, j)){
+             
+                  dp[i] = Math.min(dp[i], 1 + dp[j+1]);
+               }
+            }
+        }
+        
+        return dp[0]-1// Minus one because we don't partition at the end. s="a" == 0;
+        
+    }
+
+    public boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
