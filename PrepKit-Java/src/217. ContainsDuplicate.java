99e44badb801d9000db98afbdb01be21ee75379e
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2022-10-21 00:28:59 -0400

    Contains Duplicate i

---
+++ PrepKit-Java/src/217. ContainsDuplicate.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hashset = new HashSet();
+        for (int i = 0; i < nums.length; i++) {
+            if (!hashset.add(nums[i]))
+                return true;
+        }
+        return false;
+    }
+}
