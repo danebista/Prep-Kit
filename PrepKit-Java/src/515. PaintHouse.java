b4a7d897c0f3d5f5009fe036541465a47bf842f4
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2022-10-24 06:44:02 -0400

    PH

--- PrepKit-Java/src/515. PaintHouse.java
+++ PrepKit-Java/src/515. PaintHouse.java
@@ -30,3 +30,25 @@ public class Solution {
         return minCost;
     }
 }
+
+// 滚动数组法优化空间
+public class Solution {
+    /*
+     * @param costs: n x 3 cost matrix
+     * 
+     * @return: An integer, the minimum cost to paint all houses
+     */
+    public int minCost(int[][] costs) {
+        // write your code here
+        if (costs.length == 0)
+            return 0;
+
+        for (int i = 1; i < costs.length; i++) {
+            costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
+            costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
+            costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
+        }
+
+        return Math.min(costs[costs.length - 1][0], Math.min(costs[costs.length - 1][1], costs[costs.length - 1][2]));
+    }
+}
