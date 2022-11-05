474da25b8829dc94e46008769333b90218e4d147
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2022-11-05 18:27:33 -0400

    flatten binary tree to linked list

--- PrepKit-Java/src/FlattenABinaryTreeToALinkedList.java
+++ PrepKit-Java/src/FlattenABinaryTreeToALinkedList.java
@@ -1,46 +1,35 @@
-public class FlattenABinaryTreeToALinkedList {
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        while (root != null) {
+            if (root.left != null) {
+                TreeNode prev = root.left;
 
-    public static void main(String args[]) {
-        TreeNode root = new TreeNode(1);
-        TreeNode copy = root;
-        for (int i = 2; i <= 5; i++) {
-            if (i % 2 == 0)
-                root.left = new TreeNode(i);
-            if (i % 2 == 1 && i != 5) {
-                root.right = new TreeNode(i);
-                root.right.left = new TreeNode(5);
-                root.right.right = new TreeNode(6);
-                root = root.left;
-            }
-            if (i == 5) {
-                root.right = new TreeNode(i);
-            }
+                while (prev.right != null) {
+                    prev = prev.right;
+                }
 
-        }
-
-    }
-
-    public static void flattenedTree(TreeNode root) {
-        while (root.left != null) {
-            TreeNode prev = root.left;
-            while (prev.right != null) {
                 prev.right = root.right;
-                root.left = null;
                 root.right = root.left;
+                root.left = null;
             }
             root = root.right;
+
         }
-    }
-}
 
-class TreeNode {
-    int value;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int val) {
-        this.value = val;
-        this.left = null;
-        this.right = null;
     }
 }
