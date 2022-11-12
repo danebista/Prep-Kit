3c3d1a16b45193ce276daddfc07be7c16e03c2ba
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2022-11-12 02:41:49 -0500

    Find Median From DataStream

---
+++ PrepKit-Java/src/295. FindMedianFromDataStream.java
@@ -0,0 +1,41 @@
+class MedianFinder {
+    PriorityQueue<Integer> maxHeap;
+    PriorityQueue<Integer> minHeap;
+
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<Integer>((a, b) -> (b - a));
+        minHeap = new PriorityQueue<Integer>();
+    }
+
+    public void addNum(int num) {
+        if (maxHeap.isEmpty() || maxHeap.peek() >= num) {
+
+            maxHeap.offer(num);
+        } else {
+            minHeap.offer(num);
+        }
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.offer(maxHeap.poll());
+        } else if (minHeap.size() > maxHeap.size() + 1) {
+            maxHeap.offer(minHeap.poll());
+        }
+
+    }
+
+    public double findMedian() {
+
+        if (minHeap.size() > maxHeap.size())
+            return minHeap.peek();
+        else if (maxHeap.size() > minHeap.size())
+            return maxHeap.peek();
+        else
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
