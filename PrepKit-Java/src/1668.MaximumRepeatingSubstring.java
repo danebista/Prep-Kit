c2bd7cde7b0f365826c812b3ba59053eb35fc9bb
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2022-11-24 00:16:46 -0500

    Max Repeating Substring

---
+++ PrepKit-Java/src/1668.MaximumRepeatingSubstring.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int repeating = 0;
+        StringBuilder sb = new StringBuilder(word);
+        while (sequence.contains(sb)) {
+            repeating++;
+            sb.append(word);
+        }
+        return repeating;
+    }
+
+}
