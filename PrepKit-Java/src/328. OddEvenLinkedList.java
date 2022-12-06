eb2a2a4b1e5f3a3a4fdb131b9ae8ab8835ce3335
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2022-12-06 11:08:56 -0500

    Bucket sort

--- PrepKit-Java/src/328. OddEvenLinkedList.java
+++ PrepKit-Java/src/328. OddEvenLinkedList.java
@@ -8,6 +8,35 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+Select tags
+0/5
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        ListNode odd= head;
+        if(head==null) return null;
+        ListNode even = head.next;
+        ListNode evenStart= even;
+        //Remember doing this at even indices helps both even and odd cases
+        while (even!=null && even.next!=null){
+            odd.next=odd.next.next;
+            even.next=even.next.next;
+            odd=odd.next;
+            even = even.next;
+        }
+        odd.next=evenStart;
+        return head;
+    }
+}
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         ListNode odd = new ListNode();
