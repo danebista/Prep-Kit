9033a2110e9fcbe3979b91879ea380623508f581
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2022-12-06 04:26:40 -0500

    Two St Close

---
+++ PrepKit-Java/src/1657. DetermineIfTwoStringsAreClose.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int[] word1Count = new int[26];
+        int[] word2Count = new int[26];
+        if (word1.length() != word2.length())
+            return false;
+        for (int i = 0; i < word1.length(); i++) {
+            word1Count[word1.charAt(i) - 'a']++;
+            word2Count[word2.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if ((word1Count[i] != 0 && word2Count[i] == 0)
+                    || (word2Count[i] != 0 && word1Count[i] == 0))
+                return false;
+        }
+        HashMap<Integer, Integer> hashmap = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            if (word1Count[i] != 0) {
+                hashmap.put(word1Count[i], hashmap.getOrDefault(word1Count[i], 0) + 1);
+            }
+            if (word2Count[i] != 0) {
+                hashmap.put(word2Count[i], hashmap.getOrDefault(word2Count[i], 0) - 1);
+            }
+        }
+
+        for (Integer value : hashmap.values()) {
+            if (value != 0)
+                return false;
+        }
+        return true;
+    }
+}
