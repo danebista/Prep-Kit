16e5ac70989271b020ffd2780aa1a8f6e77d3ee7
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2022-12-10 01:50:22 -0500

    Max Diff Node Ancestor

--- PrepKit-Java/src/1026. MaximumDifferenceBetweenNodeAndAncestor.java
+++ PrepKit-Java/src/1026. MaximumDifferenceBetweenNodeAndAncestor.java
@@ -1,17 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
 class Solution {
-    public int maxAncestorDiff(TreeNode root) {
+    int result;
 
-        return helper(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
+    public int maxAncestorDiff(TreeNode root) {
+        result = 0;
+        helper(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        return result;
     }
 
-    public int helper(TreeNode root, int min, int max) {
+    public void helper(TreeNode root, int min, int max) {
         if (root == null) {
-            return max - min;
+            result = Math.max(result, max - min);
+            return;
         }
         max = Math.max(max, root.val);
         min = Math.min(min, root.val);
-        int left = helper(root.left, min, max);
-        int right = helper(root.right, min, max);
-        return Math.max(left, right);
+        helper(root.left, min, max);
+        helper(root.right, min, max);
+
     }
 }
