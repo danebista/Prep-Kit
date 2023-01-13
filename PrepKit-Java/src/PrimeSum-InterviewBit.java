a63c1735dd2e5de3ba08e592a8228f445e9de315
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2023-01-13 12:43:36 -0500

    Prime Sum

---
+++ PrepKit-Java/src/PrimeSum-InterviewBit.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int[] primesum(int A) {
+        if (A%2==1){
+            int checker = A-2;
+            if (isPrime(checker)) return new int[]{2, checker};
+        }
+        else {
+            if (A==4) return new int[]{2,2};
+            for (int i=3; i*i<=A; i++){
+                if (isPrime(i) && isPrime(A-i)){
+                    return new int[]{i,A-i};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+    
+    public int isPrime(int num){
+        if (n==1) return false;
+        for (int i=2; i*i<=num; i++){
+            if (num%i==0) return false;
+        }
+        return true;
+    }
+}
