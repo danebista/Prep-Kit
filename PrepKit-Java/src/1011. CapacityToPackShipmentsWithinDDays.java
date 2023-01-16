3ea7a85c901c4b3fe9c8010ef4ef930465c58ef3
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2023-01-16 17:40:23 -0500

    Capacity Pack

---
+++ PrepKit-Java/src/1011. CapacityToPackShipmentsWithinDDays.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i=0; i< weights.length; i++){
+            sum+=weights[i];
+            min = Math.min(min, weights[i]);
+        }
+        int low = min;
+        int high = sum;
+        int result = Integer.MAX_VALUE;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if (isPossible(weights, days, mid)){
+                result = Math.min(result, mid);
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return result;
+    }
+
+    public boolean isPossible(int[] weights, int days, int mid){
+        int count = 1;
+        int start = 0;
+        for (int i=0; i< weights.length; i++){
+            if (weights[i]> mid) return false;
+            start+=weights[i];
+            if (start>mid){
+                count++;
+                if (count> days) return false;
+                start=weights[i];
+            }
+        }
+        return true;
+    }
+}
