c16c10db9b374e984e2f63a4a9f96a1c42331ed9
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2023-01-31 22:22:26 -0500

    Add Two Numbers

---
+++ PrepKit-Java/src/1-10/2. AddTwoNumbers.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode newThird = new ListNode(0);
+        ListNode temp = newThird;
+        int carry = 0;
+        while(l1!=null || l2!=null){
+            int value1 = l1 == null ? 0 : l1.val;
+            int value2 = l2 == null ? 0 : l2.val;
+            
+            int sum = value1+value2+carry;
+            newThird.next = new ListNode(sum%10);
+            carry = sum /10;
+            l1=l1!=null?l1.next:null;
+            l2=l2!=null?l2.next:null;
+            newThird = newThird.next;
+        }
+        
+        if (carry != 0) newThird.next = new ListNode(carry);
+        
+        return temp.next;
+    }
+}
