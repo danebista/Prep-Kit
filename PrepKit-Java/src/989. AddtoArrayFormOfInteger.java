f67902a6d580e69580f7f931fe39cdd1167e330e
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2023-02-22 19:06:43 -0500

    Rate Logger

--- PrepKit-Java/src/989. AddtoArrayFormOfInteger.java
+++ PrepKit-Java/src/989. AddtoArrayFormOfInteger.java
@@ -1,31 +1,31 @@
-class Solution {
-    public List<Integer> addToArrayForm(int[] A, int K) {
-        List<Integer> list = new ArrayList();
-        int n = A.length;
-        int carry = 0;
-        for(int i=n-1; i>=0; i--){
-            carry += K%10 + A[i];
-            list.add(carry%10);
-            carry/=10;
-            K/=10;
+    class Solution {
+        public List<Integer> addToArrayForm(int[] A, int K) {
+            List<Integer> list = new ArrayList();
+            int n = A.length;
+            int carry = 0;
+            for(int i=n-1; i>=0; i--){
+                carry += K%10 + A[i];
+                list.add(carry%10);
+                carry/=10;
+                K/=10;
+            }
+            while(K!=0){
+                carry+=K%10;
+                list.add(carry%10);
+                carry/=10;
+                K/=10;
+            }
+            if(carry>0){
+                list.add(carry);
+            }
+            int size = list.size();
+            int l = 0, r = size-1;
+            while(l<r){
+                int tmp = list.get(l);
+                list.set(l,list.get(r));
+                list.set(r,tmp);
+                l++;r--;
+            }
+            return list;
         }
-        while(K!=0){
-            carry+=K%10;
-            list.add(carry%10);
-            carry/=10;
-            K/=10;
-        }
-        if(carry>0){
-            list.add(carry);
-        }
-        int size = list.size();
-        int l = 0, r = size-1;
-        while(l<r){
-            int tmp = list.get(l);
-            list.set(l,list.get(r));
-            list.set(r,tmp);
-            l++;r--;
-        }
-        return list;
     }
-}
