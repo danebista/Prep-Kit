81019154dd899b5a1615b6b334eba611003ecb85
Dinank Bista <dinankbista@Dinanks-MacBook-Pro.local>
2023-03-12 18:17:39 -0400

    Maximum attend

--- PrepKit-Java/src/1353.MaximumNumberOfEventsThatCanBeAttended.java
+++ PrepKit-Java/src/1353.MaximumNumberOfEventsThatCanBeAttended.java
@@ -18,3 +18,41 @@ public class MaximumNumberOfEventsThatCanBeAttended {
         }
     }
 }
+
+class Solution {
+
+    public int maxEvents(int[][] events) {
+        PriorityQueue<int[]> allEvents = new PriorityQueue<int[]>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+    
+        PriorityQueue<int[]> curEvents = new PriorityQueue<int[]>((a, b) -> a[1] - b[1]);
+    
+        for (var event : events) {
+            allEvents.add(event);
+        }
+        //curEvents.add(allEvents.poll());
+        int curTime = allEvents.peek()[0];
+        int count = 0;
+        while (allEvents.size() > 0 || curEvents.size() > 0) {
+            while (allEvents.size() > 0 && allEvents.peek()[0] <= curTime && allEvents.peek()[1] >= curTime) {
+                curEvents.add(allEvents.poll());
+            }
+    
+            if (curEvents.size() > 0) {
+                var next = curEvents.poll();
+                if (next[1] >= curTime) {
+                    count++;
+                    curTime++;
+                }
+            } else {
+                if (allEvents.size() > 0) {
+                    curTime = allEvents.peek()[0];
+                }
+            }
+        }
+        return count;
+    }
+    }
+    
+    
+    
+    
